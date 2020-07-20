18f95f3267035d24490db3c05733f4dbbdafff9d
Mario Alonso <mario.blazter99@gmail.com>
2020-07-20 18:37:55 -0500

    Edición y Eliminación de Registros de Alumnos, Módulo Dirección

--- apps/appDirection/students/urls.py
+++ apps/appDirection/students/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.appDirection.students.views import StudentCreate, StudentList
+from apps.appDirection.students.views import StudentCreate, StudentList, StudentUpdate, StudentDelete
 
 studentpatterns = ([
     path('new/', StudentCreate.as_view(), name='student_create'),
     path('list/', StudentList.as_view(), name='student_list'),
+    path('update/<int:pk>/', StudentUpdate.as_view(), name='student_update'),
+    path('delete/<int:pk>/', StudentDelete.as_view(), name='student_delete'),
 ], 'students')
--- apps/appDirection/students/views.py
+++ apps/appDirection/students/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 
 from apps.appDirection.students.models import Student
@@ -16,3 +16,14 @@ class StudentList(ListView):
     queryset = Student.objects.order_by('especialidad', 'cuatrimestre', 'grupo')
     template_name = 'students/students_list.html'
     paginate_by = 30
+
+class StudentUpdate(UpdateView):
+	model = Student
+	form_class = StudentForm
+	template_name = 'students/student_formCreate.html'
+	success_url = reverse_lazy('students:student_list')
+
+class StudentDelete(DeleteView):
+	model = Student
+	template_name = 'students/student_delete.html'
+	success_url = reverse_lazy('students:student_list')
