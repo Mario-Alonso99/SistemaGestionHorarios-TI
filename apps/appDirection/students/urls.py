d1e085fc0f560a6162f3f7ed7a217575993750b8
Mario Alonso <mario.blazter99@gmail.com>
2020-07-20 18:25:44 -0500

    Consulta y listado de Alumnos, Módulo Dirección

--- apps/appDirection/students/urls.py
+++ apps/appDirection/students/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.appDirection.students.views import StudentCreate
+from apps.appDirection.students.views import StudentCreate, StudentList
 
 studentpatterns = ([
     path('new/', StudentCreate.as_view(), name='student_create'),
+    path('list/', StudentList.as_view(), name='student_list'),
 ], 'students')
--- apps/appDirection/students/views.py
+++ apps/appDirection/students/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 
 from apps.appDirection.students.models import Student
@@ -11,3 +11,8 @@ class StudentCreate(CreateView):
     form_class = StudentForm
     template_name = 'students/student_formCreate.html'
     success_url = reverse_lazy('students:student_list')
+
+class StudentList(ListView):
+    queryset = Student.objects.order_by('especialidad', 'cuatrimestre', 'grupo')
+    template_name = 'students/students_list.html'
+    paginate_by = 30
