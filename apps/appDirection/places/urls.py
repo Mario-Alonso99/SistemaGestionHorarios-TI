e0e61d44d3798ae3bcb64bfec82ae4e85a5d377f
Mario Alonso <mario.blazter99@gmail.com>
2020-07-21 17:00:09 -0500

    Reporte en formato xlsx de Aulas/Laboratorios, Módulo Dirección

--- apps/appDirection/places/urls.py
+++ apps/appDirection/places/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.appDirection.places.views import PlaceCreate, PlaceList, PlaceUpdate, PlaceDelete, PlaceShow, search
+from apps.appDirection.places.views import PlaceCreate, PlaceList, PlaceUpdate, PlaceDelete, PlaceShow, search, PlaceReport
 
 placepatterns = ([
     path('new/', PlaceCreate.as_view(), name='place_create'),
@@ -7,7 +7,6 @@ placepatterns = ([
     path('update/<int:pk>/', PlaceUpdate.as_view(), name='place_update'),
     path('delete/<int:pk>/', PlaceDelete.as_view(), name='place_delete'),
     path('show/<int:pk>/', PlaceShow.as_view(), name='place_show'),
-    path('search/', search, name='place_search')
-    #path('report/', StudentReport.as_view(), name='report'),
-    #path('import/', importar, name="student_import")
+    path('search/', search, name='place_search'),
+    path('report/', PlaceReport.as_view(), name='report'),
 ], 'places')
