2e8431ecd469886ba54cfac31972b893f31c037c
Mario Alonso <mario.blazter99@gmail.com>
2020-07-20 20:41:33 -0500

    Carga Masiva y Generación de resportes de Alumnos, Módulo Dirección

--- apps/appDirection/students/urls.py
+++ apps/appDirection/students/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from apps.appDirection.students.views import StudentCreate, StudentList, StudentUpdate, StudentDelete, StudentShow, search
+from apps.appDirection.students.views import StudentCreate, StudentList, StudentUpdate, StudentDelete, StudentShow, search, StudentReport
+from .views import importar 
 
 studentpatterns = ([
     path('new/', StudentCreate.as_view(), name='student_create'),
@@ -8,4 +9,6 @@ studentpatterns = ([
     path('delete/<int:pk>/', StudentDelete.as_view(), name='student_delete'),
     path('show/<int:pk>/', StudentShow.as_view(), name='student_show'),
     path('search/', search, name='student_search'),
+    path('report/', StudentReport.as_view(), name='report'),
+    path('import/', importar, name="student_import")
 ], 'students')
