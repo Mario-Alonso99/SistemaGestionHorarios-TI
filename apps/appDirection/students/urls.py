3e07a08ee694bc9b5ba77aa66084fa439e2aa3f6
Mario Alonso <mario.blazter99@gmail.com>
2020-07-28 17:55:55 -0500

    Implementación del Nuevo Módelo de Alumnos al Master y Pruebas

--- apps/appDirection/students/urls.py
+++ apps/appDirection/students/urls.py
@@ -11,8 +11,8 @@ from .views import importar
 
 studentpatterns = ([
     path('new/', StudentCreate.as_view(), name='student_create'),
-    path('list/',StudentList.as_view(), name='student_list'),
-    path('update/<int:pk>/', StudentUpdate.as_view(), name='student_update'),
+    path('list/', StudentList.as_view(), name='student_list'),
+    path('update/<int:pk>/', login_required (StudentUpdate.as_view()), name='student_update'),
     path('delete/<int:pk>/', login_required (StudentDelete.as_view()), name='student_delete'),
     path('show/<int:pk>/', login_required (StudentShow.as_view()), name='student_show'),
     path('search/', login_required (search), name='student_search'),
